1f3e01a6e816765fd1b8c6aeb09018e7569f45b7
Ahmad Rifai <70496058+ahmdriffai@users.noreply.github.com>
2022-09-12 14:26:49 +0700

    clg

--- resources/views/layouts/components/menu/penjaminan-mutu.blade.php
+++ resources/views/layouts/components/menu/penjaminan-mutu.blade.php
@@ -19,7 +19,7 @@
 @endrole
 
 
-@php($penjaminanMutu = \App\Models\PenjaminanMutu::orderBy('nama', 'ASC')->paginate(100))
+@php($penjaminanMutu = \App\Models\PenjaminanMutu::orderBy('id', 'ASC')->paginate(100))
 
 @foreach($penjaminanMutu as $value)
 <li class="menu-item">
